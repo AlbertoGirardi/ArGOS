0d04be1fa68c43d65625b152c11f47d876bf06ce
Alberto Girardi <alberto.gir747@gmail.com>
2023-09-23 23:21:23 +0200

    digit printing function

--- src/boot.asm
+++ src/boot.asm
@@ -8,9 +8,11 @@ jmp main
 
 
 nl:
-    
+    push si
     mov si, newline
     call print
+
+    pop si
     
     ret
 
@@ -21,7 +23,7 @@ print:                 ;prints what is in si
 
 .print_loop:
 
-    lodsb
+    lodsb               ;load the content of si
 
     or al, al
     jz .print_done
@@ -31,10 +33,37 @@ print:                 ;prints what is in si
     jmp .print_loop
 
 
-.print_done:
+.print_done:     
+
+       ; return
     pop ax
     pop si
     ret
+
+
+print_digit: 
+
+
+             ;print a single number value given in bh,
+    push si
+    push ax
+    push bx
+
+    mov bx, 48
+    add si, bx
+    
+    mov ax, si
+
+    mov ah, 0x0e
+    int 0x10
+
+
+    pop bx
+    pop ax
+    pop si
+
+    ret
+    
 
 
 main:
@@ -44,19 +73,32 @@ main:
     mov es, ax
 
 
-    call nl
-    call nl
-    call nl
+    times 3 call nl
 
     mov si, msg_ARGOS   ; welcome message
     call print
 
-    call nl
-    call nl
-
+    times 2 call nl
 
     mov si, msg
     call print
+
+    times 2 call nl
+
+    mov bx, 3
+
+
+    mov si, 5
+   
+    call print_digit
+    call nl
+    mov si, bx 
+    call print_digit
+
+    
+
+
+  
 
    
 
@@ -65,15 +107,18 @@ main:
 
 
 
-msg_ARGOS: db "         ArGOS", 0
-msg: db "BOOTLOADER. OS booting start", ENDL,"benvenuti", 0
+msg_ARGOS: db "                 ArGOS", 0
+msg: db "BOOTLOADER. OS booting start", ENDL,"Benvenuti! Alcuni test in assembly",ENDL,0
 
 newline: db 10, 13, 0
 
 
+
 .halt:
+  
+   
     jmp .halt
 
-times 510 -  ($ - $$) db 0
+times 510 -  ($-$$)  db 0
 
 db 0x55, 0xaa
