87016f038c82b900512934d5a00d8fb3e9f2edfa
Alberto Girardi <alberto.gir747@gmail.com>
2023-10-15 00:38:22 +0200

    stage 2 test

--- src/boot32.asm
+++ src/boot32.asm
@@ -5,19 +5,23 @@
 ;;;SECOND STAGE OF THE BOOTLOADER,  
 
 
-
+var2: dw 7653
 
 B32:
 
-    push 351
-
-    call print_number
-    call nl
 
     mov si, [var]
     push si
 
     call print_number
+
+    call nl
+
+    mov si, [var2]
+    push si
+
+    call print_number
+
 
 
 jmp CLOSURE
