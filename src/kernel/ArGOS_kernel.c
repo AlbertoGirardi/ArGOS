98d1ceb066827d0e0ee45668ea07f809a1394986
Alberto Girardi <alberto.gir747@gmail.com>
2023-10-25 22:34:07 +0200

    fun with c code

--- src/kernel/ArGOS_kernel.c
+++ src/kernel/ArGOS_kernel.c
@@ -1,23 +1,35 @@
 //KERNEL OF ARGOS OPERATING SYSTEM
 
 
+int function(int n){
 
-extern void main(){
+    n = 0xb8070+n*2;
+    for (char *p = (char *)0xb8070; p < (char *)n; p += 1)
+    {
+        *p = (char)'b';
+        p++;
+        *p = (char)0x1f;
+    }
+
+    return 0;
+
+}
 
 
-    *(char*)0xb8002 = 'C';
-    
-    *(char*)0xb8000 = 'C';
-     *(char*)0xb8004 = 'C';
-    
-    *(char*)0xb8008 = 'C';
-     *(char*)0xb800A = 'C';
-    
-    *(char*)0xb800C = 'C';
-     *(char*)0xb8006 = 'C';
-    
-    *(char*)0xb8000 = 'C';
-   
+
+extern void main()
+{
+
+    *(char *)0xb8000 = 'C';
+
+    for (char *p = (char *)0xb8002; p < (char *)0xb805F; p += 1)
+    {
+        *p = (char)'C';
+        p++;
+        *p = (char)0x1f;
+    }
+
+    function(80);
+
     return;
-
 }
